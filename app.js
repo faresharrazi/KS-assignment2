5f0d85b7e8ae2c7e3349082ae608f561eefd7ff2
Thanzia <shaikhtanz@gmail.com>
2024-02-08 18:21:56 +0800

    fixed user input validation

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   // Check for valid playerSelection
-  if (
-    playerSelection !== "rock" &&
-    playerSelection !== "paper" &&
-    playerSelection !== "scissors"
-  ) {
-    return "Invalid selection. Please choose Rock, Paper, or Scissors.";
-  }
+  //   if (
+  //     playerSelection !== "rock" &&
+  //     playerSelection !== "paper" &&
+  //     playerSelection !== "scissors"
+  //   ) {
+  //     return "Invalid selection. Please choose Rock, Paper, or Scissors.";
+  //   }
 
   // Determine the winner of the round
   if (playerSelection === computerSelection) {
@@ -51,29 +51,69 @@ console.log(playRound(playerSelection, computerSelection));
 console.log("Refresh for a new round");
 
 // Step e:
-console.log("Step e starts here");
-// prompt()
-const playerSelection = prompt(
-  `Input your choice! "Rock 🤜🏼" "Paper 👋🏼" "Scissors ✌🏼"`
-).toLowerCase();
-switch (playerSelection) {
-  case "rock":
-    console.log(
-      `Your choice is ${playerSelection} and computer\'s choice is ${computerSelection}`
+
+function inputValidation() {
+  let selection = prompt(`Input your choice! "Rock" "Paper" "Scissors"`);
+  if (selection === null) {
+    console.log("User canceled the game. 🥲");
+    return null;
+  }
+  selection = selection.toLowerCase();
+  while (
+    selection !== "rock" &&
+    selection !== "paper" &&
+    selection !== "scissors"
+  ) {
+    selection = prompt(
+      `Wrong input, try again!🥴 Please enter "Rock" "Paper" "Scissors"`
     );
-    break;
-  case "paper":
-    console.log(
-      `Your choice is ${playerSelection} and computer\'s choice is ${computerSelection}`
-    );
-    break;
-  case "scissors":
-    console.log(
-      `Your choice is ${playerSelection} and computer\'s choice is ${computerSelection}`
-    );
-    break;
-  default:
-    console.log(
-      prompt(`Wrong input, try again! 🥴 "Rock 🤜🏼" "Paper 👋🏼" "Scissors ✌🏼"`)
-    );
+    if (selection === null) {
+      console.log("User canceled the game.🥲");
+      return null;
+    }
+    selection = selection.toLowerCase();
+  }
+  return selection;
 }
+
+function game() {
+  let userWin = 0;
+  let computerWin = 0;
+
+  for (let i = 0; i < 5; i++) {
+    console.log(`Round ${i + 1}`);
+    const playerSelection = inputValidation();
+
+    if (playerSelection === null) {
+      console.log("Game canceled.");
+      return;
+    }
+    const computerSelection = computerPlay();
+    console.log(
+      `Your choice is ${playerSelection} and computer\'s choice is ${computerSelection}`
+    );
+    const resultLog = playRound(playerSelection, computerSelection);
+    console.log(resultLog);
+
+    if (resultLog.includes("You win!")) {
+      userWin += 1;
+    } else if (resultLog.includes("You lose!")) {
+      computerWin += 1;
+    }
+  }
+
+  //final result
+  if (userWin > computerWin) {
+    console.log(
+      "%cYou won the GAME and saved the world 😀 You are a hero!",
+      "color: green; font-size: 16px"
+    );
+  } else if (userWin < computerWin) {
+    console.log(
+      "%cOh no, the computer won 😈 and will now take over the world",
+      "color: blue; font-size: 16px"
+    );
+  }
+  console.log("refresh the page to start a new game");
+}
+game();
